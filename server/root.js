944f9a3054e882e27c80c60d4767ae65c6b42577
John Meyer <jmeyer5@nd.edu>
2020-04-14 23:19:29 -0400

    Set JWT token upon login and access it in subsequent requests
    * Save the jwt token in a cookie and get in other requests
    * Fix CORS cookie issue

--- server/root.js
+++ server/root.js
@@ -59,7 +59,7 @@ let queries = {
             data: results.rows[0]['SALPERS_NAME']
         }
     },
-    login: async ({ email, password }, context) => { // can access user info through context
+    login: async ({ email, password }, { req, res }) => { // can access user info through context
         let results = await run(
             'select password from users where user_id = :email',
             [email]
@@ -80,11 +80,13 @@ let queries = {
             let token = jwt.sign({
                 id: email,
                 email: email,
-            }, 'shouldchangethis', { expiresIn: 60 }) /// 60 is for 60 seconds, can enter 1w, 1y, 60 * 60, etc
+            }, 'shouldchangethis', { expiresIn: 3600000 }) /// 60 is for 60 seconds, can enter 1w, 1y, 60 * 60, etc
+
+            res.cookie('jwtAuth', token, {maxAge: 3600000, httpOnly: true}); //TODO secure: true
+            
             return {
                 failure: false,
-                message: 'Successfully logged in',
-                data: token
+                message: 'Successfully logged in'
             };
         }
         else { // indicates nonexistent username
