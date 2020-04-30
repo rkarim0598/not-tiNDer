f5e06e075a08e7e42ee0a33b8507bcedf6206c80
rkarim0598 <34196211+rkarim0598@users.noreply.github.com>
2020-04-30 09:46:48 -0400

    Feature/bottom bar (#15)
    * Add initial bottom bar
    
    * Add bottom nav bar

---
+++ app/src/components/BottomNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout class="bottom-nav" align-end justify-end>
+    <v-bottom-navigation class="fill-width" :value="data.activeIndex" grow color="teal" dark>
+      <v-btn v-for="tab in data.tabs" :key="tab.title">
+        <span>{{ tab.title }}</span>
+        <v-icon>{{ tab.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "BottomNav",
+  props: {
+    data: Object
+  }
+}
+</script>
+<style lang="scss" scoped>
+.bottom-nav {
+  display: flex;
+  align-self: flex-end;
+  width: 100%;
+  height: 56px !important;
+}
+</style>
